d08b64d7507c0f144f71af1f7f0d5ed7e3fe219a
jeiling <a0983681642@gmail.com>
2025-02-21 15:53:07 +0800

    chore: Update environment variable setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['openweathermap.org'], 
+  },
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+  },
 };
 
 export default nextConfig;
