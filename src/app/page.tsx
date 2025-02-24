969ab1b08d72fd6bd00196d6f94579cb11d7a22d
jeiling <a0983681642@gmail.com>
2025-02-24 11:56:22 +0800

    fix: replace dnd lib for touch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-'use client'
-import React from 'react';
-import WeatherForm from './components/WeatherForm';
-import WeatherCard from './components/WeatherCard';
-import useWeather from './hooks/useWeather';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+"use client";
+import React from "react";
+import WeatherForm from "./components/WeatherForm";
+import WeatherCard from "./components/WeatherCard";
+import useWeather from "./hooks/useWeather";
+import { DndProvider } from "react-dnd";
+import {  MultiBackend } from "react-dnd-multi-backend";
+import { HTML5toTouch } from 'rdndmb-html5-to-touch' 
 
 const WeatherPage = () => {
-  const { loading, weatherCards, handleAddWeatherCard, handleDeleteWeatherCard, setWeatherCards } = useWeather();
+  const {
+    loading,
+    weatherCards,
+    handleAddWeatherCard,
+    handleDeleteWeatherCard,
+    setWeatherCards,
+  } = useWeather();
 
   const moveCard = (dragIndex: number, hoverIndex: number) => {
     const draggedCard = weatherCards[dragIndex];
@@ -18,13 +25,16 @@ const WeatherPage = () => {
   };
 
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={MultiBackend} options={HTML5toTouch}>
       <div className="min-h-screen bg-gradient-to-r from-blue-400 to-purple-500 p-4 flex items-center justify-center">
         <div className="max-w-2xl w-full bg-white p-6 rounded-lg shadow-lg">
           <h1 className="text-4xl font-bold text-center mb-6 text-gray-800">
             Weather App
           </h1>
-          <WeatherForm onAddWeatherCard={handleAddWeatherCard} loading={loading} />
+          <WeatherForm
+            onAddWeatherCard={handleAddWeatherCard}
+            loading={loading}
+          />
           <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {weatherCards.map((weatherData, index) => (
               <WeatherCard
